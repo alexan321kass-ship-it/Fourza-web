6299522e636d087d00236a36b8e39eb74b0f04f5
merchan <alexan321kass@gmail.com>
2026-09-14 03:32:37 -0500

    fix(mobile): adjust Mission & Vision cards height and padding to prevent text overflow on mobile screens

--- js/script.js
+++ js/script.js
@@ -68,6 +68,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
 
+    document.querySelectorAll(".flip-card").forEach(card => {
+        card.addEventListener("click", () => {
+            card.classList.toggle("flipped");
+        });
     });
 
     /* ==========================================
